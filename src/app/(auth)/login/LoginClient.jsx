b66ea1024d7af10fc4e5601863e13897787213c5
parkdonggun <hbd9425@gmail.com>
2023-08-29 20:08:43 +0900

    input 컴포넌트 추가 및 스타일링

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -6,6 +6,7 @@ import LogoPath from "@/assets/colorful.svg";
 import { useRouter } from "next/navigation";
 import Loader from "@/components/loader/Loader";
 import styles from "./Auth.module.scss";
+import Input from "../../../components/input/Input";
 
 const LoginClient = () => {
   const [email, setEmail] = useState("");
@@ -28,7 +29,7 @@ const LoginClient = () => {
 
   return (
     <>
-      <Loader />
+      {isLoading && <Loader />}
       <section className={styles.page}>
         <div className={styles.container}>
           <h1 className={styles.logo}>
@@ -36,7 +37,28 @@ const LoginClient = () => {
           </h1>
 
           <form onSubmit={loginUser} className={styles.form}>
-            input
+            <Input
+              email
+              icon="letter"
+              id="email"
+              name="email"
+              label="이메일"
+              placeholder="아이디(이메일)"
+              className={styles.control}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              password
+              icon="lock"
+              id="password"
+              name="password"
+              label="비밀번호"
+              placeholder="비밀번호"
+              className={styles.control}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <div className={styles.group}>자동 로그인, 비밀번호 수정</div>
             <div className={styles.buttonGroup}>
               button
